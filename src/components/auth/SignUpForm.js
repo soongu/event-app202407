708258d1a9dc0d7904e7014ca9d225a9ec7b590f
soongu <naong526@naver.com>
2024-07-12 09:28:59 +0900

    # 이메일 중복확인 부수처리

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SignUpForm.module.scss';
 import EmailInput from './EmailInput';
+import VerificationInput from './VerificationInput';
+import ProgressBar from '../ProgressBar';
 
 const SignUpForm = () => {
+
+  // 현재 몇 단계가 진행되고 있는지
+  const [step, setStep] = useState(1);
+
+  // 단계가 성공적으로 완료되었는지
+  const [success, setSuccess] = useState(false);
+
+  // 이메일 중복확인이 끝났을 때 호출될 함수
+  const emailSuccessHandler = () => {
+
+    setSuccess(true);
+
+    setTimeout(() => {
+      setStep(2);
+      setSuccess(false);
+    }, 1500);
+
+  };
+
   return (
     <div className={styles.signupForm}>
       <div className={styles.formStepActive}>
 
-        <EmailInput />
+        {step === 1 && <EmailInput onSuccess={emailSuccessHandler} />}
+
+        {step === 2 && <VerificationInput />}
+
+        {success && <ProgressBar />}
 
       </div>
     </div>
