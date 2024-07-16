355d68377a232a665a0eccefb46105d7cea860e4
soongu <naong526@naver.com>
2024-07-16 09:57:08 +0900

    # 로그인 조건부 렌더링

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Link, useActionData } from 'react-router-dom'; // Link 컴포넌트 추가
+import { Form, Link, redirect, useActionData } from 'react-router-dom'; // Link 컴포넌트 추가
 import styles from './LoginForm.module.scss';
 import { AUTH_URL } from '../../config/host-config';
 
@@ -72,10 +72,20 @@ export const loginAction = async ({ request }) => {
   // 실패한 경우
   if (response.status === 422) {
     const errorText = await response.text();
-    console.log(errorText);
+    // console.log(errorText);
 
     // 액션함수가 리턴한 데이터를 컴포넌트에서 쓰는법
     // 컴포넌트에서 useActionData 훅을 사용
     return errorText;
   }
+
+  // 성공한 경우
+  const responseData = await response.json();
+  // console.log(responseData);
+
+  // 브라우저 저장소 (localStorage : 쿠키, sessionStorage: 세션)
+  localStorage.setItem('userData', JSON.stringify(responseData));
+
+  return redirect('/');
+
 };
