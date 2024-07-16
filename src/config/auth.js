147c7d2891eb62f46a571c8986ab8368c51a1cd1
soongu <naong526@naver.com>
2024-07-16 10:29:40 +0900

    # loader를 이용한 인증화면 처리

---
+++ src/config/auth.js
@@ -0,0 +1,19 @@
+
+// 로그인한 유저의 정보 가져오기
+const getUserData = () => {
+  const userDataJson = localStorage.getItem('userData');
+  const userData = JSON.parse(userDataJson);
+
+  return userData;
+};
+
+// 인증 토큰만 가져오기
+export const getUserToken = () => {
+  return getUserData().token;
+};
+
+// 로그인 회원정보를 불러오는 loader
+export const userDataLoader = () => {
+  console.log('userDataLoader call!');
+  return getUserData();
+};
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import LoginForm from '../components/auth/LoginForm';
 import Main from '../components/Main';
+import { useRouteLoaderData } from 'react-router-dom';
 
 const WelcomePage = () => {
 
-  console.log('WelcomePage 실행!');
+  // console.log('WelcomePage 실행!');
+
+  const userData = useRouteLoaderData('user-data'); // 상위 라우트 페이지의 loader데이터 불러오기
+  // console.log('userData: ', userData);
 
   return (
     <>
-      { <LoginForm /> }
-      { <Main /> }
+      {!userData && <LoginForm /> }
+      {userData && <Main /> }
     </>
   );
 };
