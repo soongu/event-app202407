940eee90823d278fa5a1853433fffee25e102ec3
soongu <naong526@naver.com>
2024-07-23 15:38:02 +0900

    # github actions

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -10,8 +10,8 @@ let backendHostName;
 
 if (clientHostName === 'localhost') {
   backendHostName = 'http://localhost:' + LOCAL_PORT;
-} else if (clientHostName === 'www.bananagrape.co.kr') {
-  backendHostName = 'https://api.myapi.com';
+} else if (clientHostName === 'my-event-app998877.s3-website.ap-northeast-2.amazonaws.com') {
+  backendHostName = 'http://3.38.68.209:8989';
 }
 
 const API_BASE_URL = backendHostName;
